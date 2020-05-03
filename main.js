13953336e34b913f0f53f723163b4a835315d268
ilya <filippoknator@mail.ru>
2020-05-03 20:37:26 +0300

    start

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict';
+var money = prompt("Ваш бюджет на месяц?");
+var time = prompt("Введите дату в формате YYYY-MM-DD");
+var questionFirst = prompt("Введите обязательную статью расходов в этом месяце");
+var questionSecond = prompt("Во сколько обойдется?");
+
+var appData={
+    money:money,
+    timeData:time,
+    expenses:{
+
+    },
+    optionalExpenses:{
+
+    },
+    income:[],
+    savings:false
+};
+appData.expenses.questionFirst = questionSecond;
+console.log(appData.expenses);
+
+
+
