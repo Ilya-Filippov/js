8bca003e5c8c09c1c49040ac35c82aef826d62cd
ilya <filippoknator@mail.ru>
2020-05-03 20:38:50 +0300

    start

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var money = prompt("Ваш бюджет на месяц?");
 var time = prompt("Введите дату в формате YYYY-MM-DD");
 var questionFirst = prompt("Введите обязательную статью расходов в этом месяце");
 var questionSecond = prompt("Во сколько обойдется?");
-
 var appData={
     money:money,
     timeData:time,
@@ -16,6 +15,7 @@ var appData={
     income:[],
     savings:false
 };
+
 appData.expenses.questionFirst = questionSecond;
 console.log(appData.expenses);
 
