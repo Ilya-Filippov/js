a9a7781df7432ef3a3cc3078ad6ee2dee1389ec9
ilyanator <filippoknator@mail.ru>
2020-05-04 16:35:14 +0300

    homework

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 'use strict';
-var money = prompt("Ваш бюджет на месяц?");
-var time = prompt("Введите дату в формате YYYY-MM-DD");
-var questionFirst = prompt("Введите обязательную статью расходов в этом месяце");
-var questionSecond = prompt("Во сколько обойдется?");
-var appData={
+let arrQ = [];
+let arrA = [];
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+for (let i = 0; i <2; i++) {
+   arrQ[i] = prompt("Введите обязательную статью расходов в этом месяце");
+   arrA[i] = prompt("Во сколько обойдется?");
+ }
+ let appData={
     money:money,
     timeData:time,
     expenses:{
-
     },
     optionalExpenses:{
-
     },
     income:[],
     savings:false
 };
 
-appData.expenses.questionFirst = questionSecond;
+for (let i = 0; i < 2; i++) {
+  appData.expenses[arrQ[i]] = arrA[i];
+}
 console.log(appData.expenses);
-
-
-
+alert("Ваш бюджет на один день " + appData.money/30 + " рублей");
